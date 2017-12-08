cd6cc83c650d3766c7ff3b12ccba53643125874b
Arjan Loeffen <arjan.loeffen@armatiek.nl>
2017-12-08 10:50:08 +0100

    commons.lang3 in stead of lang

--- src/main/java/nl/imvertor/ComplyExtractor/ComplyExtractor.java
+++ src/main/java/nl/imvertor/ComplyExtractor/ComplyExtractor.java
@@ -28,7 +28,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Vector;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.http.HttpHeaders;
 import org.apache.log4j.Logger;
 
