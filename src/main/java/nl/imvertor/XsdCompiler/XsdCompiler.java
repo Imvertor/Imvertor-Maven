3102625edcb3b5060a5f21005ffdedefe2eca847
Arjan Loeffen <arjan.loeffen@armatiek.nl>
2018-10-25 10:24:47 +0200

    Bugfix

--- src/main/java/nl/imvertor/XsdCompiler/XsdCompiler.java
+++ src/main/java/nl/imvertor/XsdCompiler/XsdCompiler.java
@@ -157,7 +157,7 @@ public class XsdCompiler extends Step {
 		AnyFolder xsdFolder = new AnyFolder(configurator.getXParm("system/work-xsd-folder-path"));
 		xsdFolder.mkdirs();
 				
-		AnyFolder xsdApplicationFolder = new AnyFolder(configurator.getXParm("properties/RESULT_XSD_APPLICATION_FOLDER"));
+		AnyFolder xsdApplicationFolder = new AnyFolder(configurator.mergeParms(configurator.getXParm("properties/RESULT_XSD_APPLICATION_FOLDER")));
 		xsdApplicationFolder.mkdirs();
 		configurator.setXParm("system/xsd-application-folder-path", configurator.mergeParms(xsdApplicationFolder.getCanonicalPath()));
 		
