b99676d27de56613ca36a96b68071efb826c4c12
arjan <arjan@LAP-A-2022.fritz.box>
2024-11-16 13:01:20 +0100

    Checkpoint
    Introductie van pandoc server call.

--- src/main/java/nl/imvertor/common/file/WordFile.java
+++ src/main/java/nl/imvertor/common/file/WordFile.java
@@ -21,9 +21,15 @@
 package nl.imvertor.common.file;
 
 import java.io.File;
+import java.net.URI;
+import java.util.Base64;
+import java.util.HashMap;
 
 import org.apache.commons.exec.CommandLine;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.http.HttpHeaders;
 import org.apache.log4j.Logger;
+import org.json.JSONObject;
 
 import nl.imvertor.common.Configurator;
 import nl.imvertor.common.Runner;
@@ -54,46 +60,90 @@ public class WordFile extends AnyFile {
 	 * @param filePath
 	 * @throws Exception 
 	 */
-	public boolean toXhtmlFile(File outFile) throws Exception {
+	public boolean toXhtmlFile(XmlFile outFile) throws Exception {
 		
 		Configurator configurator = Configurator.getInstance();
 		Runner runner = configurator.getRunner();
 		boolean debugging = runner.getDebug("DOCUMENTOR");
+			
+		String pandocServerUrl = configurator.getServerProperty("pandoc.server", false);
+		if (pandocServerUrl != null) {
+			// gebruik de pandoc server: https://pandoc.org/pandoc-server.html
+			
+			HttpFile localFile = new HttpFile(this);
+			
+			HashMap<String,String> headerMap = new HashMap<String,String>();
+			//headerMap.put(HttpHeaders.AUTHORIZATION,"token " + OAUTH);
+			headerMap.put(HttpHeaders.ACCEPT, "application/octet-stream");
+			headerMap.put(HttpHeaders.CONTENT_TYPE, "application/json");
+			headerMap.put(HttpHeaders.CONTENT_ENCODING, getEncoding());
+			
+			String payloadBase64 = Base64.getEncoder().encodeToString(this.getBinaryContent());
+			String payload = "{"
+					+ "\"from\": \"docx+styles\","
+					+ "\"to\": \"html\","
+					+ "\"indented-code-classes\": ["
+						+ "\"Programmacode\""
+					+ "],"
+					+ "\"section-divs\": true,"
+					+ "\"embed-resources\": true,"
+					//+ "\"ipynb-output\": \"all\","
+					+ "\"standalone\": true,"
+					+ "\"variables\": {"
+						+ "\"lang\": \"nl-NL\","
+						+ "\"title-meta\": \"NOTITLE\""
+					+ "},"
+					+ "\"text\": \"" + payloadBase64 + "\""
+					+ "}";		
+			try {
+				String result = localFile.post(HttpFile.METHOD_POST_CONTENT, URI.create(pandocServerUrl), headerMap, null, new String[] {payload});
+				if (StringUtils.startsWith(result,"<")) {
+					outFile.setContent(result);
+					return true;
+				} else {
+					runner.error(logger, "Documentor processing error: \"" + result + "\"");
+					return false;
+				}
+			} catch (Exception e) {
+				runner.error(logger, "Documentor server error: \"" + e.getMessage() + "\"");
+			}
+			return false;
+		} else {
+			// Implementatie van Pandoc omzetting naar XHTML.
+			
+			OsExecutor osExecutor = new OsExecutor();
+			
+			String toolloc = (new AnyFile(configurator.getServerProperty("documentor.msword.transformer"))).getCanonicalPath(); // location of the tool
+			long osExecutorJobTimeout = Long.parseLong(configurator.getServerProperty("documentor.msword.transformer.timeout")); // location of the tool
+			boolean osExecutorInBackground = false;
+			
+			runner.info(logger, "Reading: " + this.getName());
+			
+			OsExecutorResultHandler osExecutorResult = null;
+			
+			/*
+			 * Dit batch file doet het volgende
+			 * - Bereid MsWord voor door roep o.a. pandoc aan en corrigeert allerlei 
+			 */
+			CommandLine commandLine = new CommandLine(toolloc + "\\documentor.bat"); // TODO: *nix
+			commandLine.addArgument(this.getName()); // the docx file
+			commandLine.addArgument(toolloc); // the tool folder
+			commandLine.addArgument(this.getParent()); // The work folder
+			commandLine.addArgument(debugging ? "true" : "false"); // debugging?
+		
+			try {
+				osExecutorResult = osExecutor.osexec(commandLine, osExecutorJobTimeout, osExecutorInBackground);
+				osExecutorResult.waitFor();
+				// assume the msword file * is transformed to *.xhtml
+				configurator.setXParm("appinfo/documentor-transformation-result", outFile.getName(),false);
+				return true;
 				
-		// Implementatie van Pandoc omzetting naar XHTML.
-		
-		OsExecutor osExecutor = new OsExecutor();
-		
-		String toolloc = (new AnyFile(configurator.getServerProperty("documentor.msword.transformer"))).getCanonicalPath(); // location of the tool
-		long osExecutorJobTimeout = Long.parseLong(configurator.getServerProperty("documentor.msword.transformer.timeout")); // location of the tool
-		boolean osExecutorInBackground = false;
-		
-		runner.info(logger, "Reading: " + this.getName());
-		
-		OsExecutorResultHandler osExecutorResult = null;
-		
-		/*
-		 * Dit batch file doet het volgende
-		 * - Bereid MsWord voor door roep o.a. pandoc aan en corrigeert allerlei 
-		 */
-		CommandLine commandLine = new CommandLine(toolloc + "\\documentor.bat"); // TODO: *nix
-		commandLine.addArgument(this.getName()); // the docx file
-		commandLine.addArgument(toolloc); // the tool folder
-		commandLine.addArgument(this.getParent()); // The work folder
-		commandLine.addArgument(debugging ? "true" : "false"); // debugging?
-			
-		try {
-			osExecutorResult = osExecutor.osexec(commandLine, osExecutorJobTimeout, osExecutorInBackground);
-			osExecutorResult.waitFor();
-			// assume the msword file * is transformed to *.xhtml
-			configurator.setXParm("appinfo/documentor-transformation-result", outFile.getName(),false);
-			return true;
-			
-		} catch (Exception e) {
-			if (osExecutorResult != null)
-				runner.error(logger, "Documentor exit value " + osExecutorResult.getExitValue() + ". " + osExecutorResult.getException().getMessage());
-			else 
-				runner.error(logger, e.getMessage());
+			} catch (Exception e) {
+				if (osExecutorResult != null)
+					runner.error(logger, "Documentor exit value " + osExecutorResult.getExitValue() + ". " + osExecutorResult.getException().getMessage());
+				else 
+					runner.error(logger, e.getMessage());
+			}
 		}
 		return false;
 		
